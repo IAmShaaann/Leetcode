d7b7bb15a4fefba994cef6d12ca6455ae5a69a91
Prashant Soni <iamshaaann@gmail.com>
2024-06-09 17:53:59 +0530

    75. Sort Colors

---
+++ 75. Sort Colors/code.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+    void sortColors(vector<int> &arr)
+    {
+        int mid = 0;
+        int start = 0;
+        int end = arr.size() - 1;
+        while (mid <= end)
+        {
+            if (arr[mid] == 0)
+            {
+                swap(arr[start], arr[mid]);
+                start++;
+                mid++;
+            }
+            else if (arr[mid] == 1)
+            {
+                mid++;
+            }
+            else if (arr[mid] == 2)
+            {
+                swap(arr[mid], arr[end]);
+                end--;
+            }
+        }
+    }
+};
