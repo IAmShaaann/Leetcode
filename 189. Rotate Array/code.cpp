2fdecb088c16339ffd7d436d13cc48064c1f96e3
Prashant Soni <iamshaaann@gmail.com>
2024-06-16 22:28:00 +0530

    189. Rotate Array

--- 189. Rotate Array/code.cpp
+++ 189. Rotate Array/code.cpp
@@ -9,13 +9,9 @@ public:
         int size = arr.size();
         k %= size;
         reverse(arr.begin(), arr.begin() + (size - k));
-        for (int it : arr)
-            cout << it << " ";
+
         reverse(arr.begin() + (size - k), arr.end());
-        for (int it : arr)
-            cout << it << " ";
+
         reverse(arr.begin(), arr.end());
-        for (int it : arr)
-            cout << it << " ";
     }
 };
